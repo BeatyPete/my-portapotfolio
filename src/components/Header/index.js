e046c81e2f2a24ec9588eb71bb0f6aa2ed610017
BeatyPete <akira941@gmail.com>
2021-03-08 04:58:37 -0600

    some styling for header and nav

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from '../Nav';
+
+function Header() {
+
+  return (
+    <header className="headContainer">
+      <h1>Lernantino</h1>
+      <Nav />
+    </header>
+  );
+}
+
+export default Header;

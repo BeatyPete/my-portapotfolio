ff88e142e1322157bdfecb412fb0b5ecd7575c43
BeatyPete <akira941@gmail.com>
2021-03-09 03:20:59 -0600

    styled main and footer created about me section and dummy sections

---
+++ src/components/Project/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Project() {
+    
+  return (
+      <h3>PRjects</h3>
+  );
+}
+
+export default Project;
---
+++ src/components/Resume/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Resume() {
+    
+  return (
+      <h3>resume</h3>
+  );
+}
+
+export default Resume;

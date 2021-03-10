8ed9196496be5390bd7ad7dc7e0cce4294009264
BeatyPete <akira941@gmail.com>
2021-03-10 05:37:15 -0600

    added project animations

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function Project() {
-    
+  const [itemFocused, setItemFocused] = useState(1);  
+
   return (
-    <main className="project-container">
-      <section>
-        <h2>The Bookfast Club</h2>
-      </section>
+    <main>
+      <div className="project-container">
+        <section className={`${itemFocused != 1 ? 'hidden': 'visible'}`} id='1' /* onMouseEnter */>
+          <div>
+            <h2>The Bookfast Club</h2>
+          </div>
+        </section>
+        <section id='2'>
+        <div>
+            <h2>The Bookfast Club</h2>
+          </div>
+        </section>
+        <section id='3'>
+        <div>
+            <h2>The Bookfast Club</h2>
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
